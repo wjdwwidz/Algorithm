5c8af30b7372802f4043ad84bc21b504addc5a8a
JeongHye <88189246+wjdwwidz@users.noreply.github.com>
2024-02-21 22:24:42 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+	int A;
+	int B;
+
+	scanf("%d", &A);
+	scanf("%d", &B);
+
+	printf("%d", A + B);
+
+	return 0;
+}
