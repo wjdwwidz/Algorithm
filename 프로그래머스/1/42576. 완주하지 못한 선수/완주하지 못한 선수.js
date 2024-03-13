07a0c7cb51bd438de19e9b8f3e91e5859ea3b3dc
JeongHye <88189246+wjdwwidz@users.noreply.github.com>
2024-03-13 22:17:11 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 47.47 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,20 @@
+function solution(participant, completion) {
+  const hashTable = {};
+
+  // completion을 해시테이블에 추가
+  for (const element of completion) {
+    if (hashTable[element]) {
+      hashTable[element]++;
+    } else {
+      hashTable[element] = 1;
+    }
+  }
+
+  // participant를 해시테이블에서 빼면서 완주하지 못한 사람 찾기
+  for (const element of participant) {
+    if (!hashTable[element]==1) { //해시테이블에 원소의 값이 없으면
+      return element; // 완주하지 못한 사람 반환
+    }
+    hashTable[element]--;
+  }
+}
