6dc139a3a9b354de73f59fde45793506cce2e084
JeongHye <88189246+wjdwwidz@users.noreply.github.com>
2024-03-29 23:14:26 +0900

    Update K번째수.py

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,9 @@
 def solution(array, commands):
     answer = []
-    for i in range(len(commands)):
-        start = commands[i][0] - 1
-        end = commands[i][1]  
-        cut = commands[i][2] - 1
-        arr = sorted(array[start:end])
-        answer.append(arr[cut])
-
+    for command in commands:
+        start, end, cut = command
+        arr = array[start-1:end]
+        arr.sort()
+        answer.append(arr[cut-1])
+        # print(answer)
     return answer
