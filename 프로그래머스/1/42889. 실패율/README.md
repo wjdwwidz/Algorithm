af0abc700e4b4baacdf82cf5c57391c2c9d53dba
JeongHye <88189246+wjdwwidz@users.noreply.github.com>
2024-04-22 23:20:22 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,6 +1,6 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=python3) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 22일 23:19:29
+2024년 04월 22일 23:20:20
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,22 +1,13 @@
-def solution(N,stages) : 
-    #스테이지 별 도전자 수 구하기
-    challanger = [0] * (N + 2)
-    for stage in stages : 
-        challanger[stage] += 1
-    
-    #스테이지별 실패한 사용자 계산 
-    fails = {}
-    total = len(stages)
+def solution(N, stages):
+    failureRates = []
+    for j in range(1, N + 1):
+        head, tail = 0, 0
+        for stage in stages:
+            if stage >= j:
+                tail += 1
+            if stage == j:
+                head += 1
+        failureRates.append(head / tail if tail != 0 else 0)
+    return failureRates
 
-    #각 스테이지를 순회하며 실패율 계산
-    for i in range(1, N + 1) :
-        if challanger[i] == 0 : #도전한 사람이 없는 경우 실패율 = 0
-            fails[i] = 0
-        else :
-            fails[i] = challanger[i] / total #실패율 구함
-            total -= challanger[i] #다음 스테이지 실패율을 구하기 위해 현재 스테이지의 인원을 뺌
-
-    #실패율이 높은 스테이지부터 내림차순으로 정렬
-    result = sorted(fails, key=lambda x : fails[x], reverse = True)
-
-    return result
+print(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]))
