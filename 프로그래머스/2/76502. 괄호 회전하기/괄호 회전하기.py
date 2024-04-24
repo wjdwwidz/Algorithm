ddd2324005c2bcee71e80f5677d21c5efd120de4
JeongHye <88189246+wjdwwidz@users.noreply.github.com>
2024-04-24 19:14:48 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,28 +1,30 @@
-def solution(s):
+def solution(s) : 
     answer = 0
     n = len(s)
-    for i in range(n):
+    for i in range(n) :
         opens = []
-        for j in range(n):
-            c = s[(i+j) % n]
-            if c == "(" or c == "[" or c == "{":
+        for j in range(n) : 
+            c = s[(i+j)%n]
+            if c == "()" or c =="[" or c =="{" : 
                 opens.append(c)
-            else:
-                if not opens:
+            else :
+                if not opens : 
                     break
 
-                # 닫힌 괄호는 스택의 top과 짝이 맞는지 비교 
-                if c == ")" and opens[-1] == "(":
-                    opens.pop()
-                elif c == "]" and opens[-1] == "[":
-                    opens.pop()
-                elif c == "}" and opens[-1] == "{":
-                    opens.pop()
-                else:
-                    break
+            #닫힌 괄호는 스택의 top과 짝이 맞는지 비교 
+            if c == "(" and opens[-1] == "(" :
+                opens.pop()
+            elif c == "]" and opens[-1] == "[" :
+                opens.pop()
+            elif c == "}" and opens[-1] == "{" :
+                opens.pop()
+            else: break
 
-        else:
+        else :
             if not opens:
                 answer += 1
-
     return answer
+
+        
+
+            
