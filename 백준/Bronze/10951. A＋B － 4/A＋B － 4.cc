2cab178647b7f3c2e2bdca316a264e8de4ab62a0
JeongHye <88189246+wjdwwidz@users.noreply.github.com>
2024-09-19 15:56:44 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<cstdio>
+
+int main() {
+
+	int A;
+	int B;
+	while (scanf("%d %d", &A, &B) != EOF)
+	{
+		int answer = A + B;
+		printf("%d\n" ,answer);
+
+	}
+
+
+	return 0;
+}
