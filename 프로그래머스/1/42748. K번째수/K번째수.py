cab789602dac07514f3fdb374ef016e72b73bced
JeongHye <88189246+wjdwwidz@users.noreply.github.com>
2024-10-30 14:30:22 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,5 @@
 def solution(array, commands):
     answer = []
     for c in commands :
-        new_array = sorted(array[c[0]-1:c[1]])
-        answer.append(new_array[c[2]-1]) 
+        answer.append(sorted(array[c[0]-1:c[1]])[c[2]-1])
     return answer
