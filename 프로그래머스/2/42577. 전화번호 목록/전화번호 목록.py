2cc75eca0e1a10b80ce6df9e143db77279ba3dc8
JeongHye <wjdwwidz@gmail.com>
2025-04-15 14:19:28 +0900

    [level 2] Title: 전화번호 목록, Time: 95.62 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,6 @@
-def solution(phoneBook):
-    phoneBook.sort()
-
-    for p1, p2 in zip(phoneBook, phoneBook[1:]):
-        if p2.startswith(p1):
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1) : 
+        if phone_book[i+1].startswith(phone_book[i]) : 
             return False
     return True
